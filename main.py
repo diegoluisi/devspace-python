296d9a8d3268b30527b2909ed2a4f418add48502
Diego Luisi <contato@diegoluisi.eti.br>
2022-03-01 20:24:06 -0300

    [🐍] add hello-python with devspace

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
-from flask import Flask
+import os
+from flask import Flask, send_from_directory
+
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -9,6 +12,13 @@ def hello():
 @app.route("/healthz")
 def healthz():
     return "UP"
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
